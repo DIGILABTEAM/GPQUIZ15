545efef3cfcebac1b2fc6fa447c62741de0fad56
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-12-03 14:51:37 +0530

    deployment

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "The most useful tool for evaluation of the condition of a patient with an acute asthmatic attack and in assessing the response to therapy is",
+				option1: "Chest radiography",
+				option2: "Arterial blood gas measurement",
+				option3: "Measurement of pulsus paradoxes",
+				option4: "Measurement of peak expiratory flow or FEV1",
+				answer: 4
+			},
+			{
+				question: "Lung compliance is increased in:",
+				option1: "Pneumonia",
+				option2: "CCF",
+				option3: "Emphysema",
+				option4: "Obesity",
+				answer: 3
+			},
+			{
+				question: "The development of a pleural effusion would be an unusual complication of",
+				option1: "Streptococcal pneumonia",
+				option2: "Staphylococcal pneumonia",
+				option3: "Mycoplasma pneumonia",
+				option4: "Pneumococcal pneumonia",
+				answer: 3
+			},
+			{
+				question: "Young asthmatic male in emergency department. Resp rate 26/min, pCO2 27, SaO2 92%, struggling, talking in sentences. Given nebulised salbutamol, and ipratropium, 200mg IV hydrocortisone. After 30 minutes - no improvement. Further management: (NEXT step)",
+				option1: "IV salbutamol",
+				option2: "IV aminophylline",
+				option3: "IV magnesium",
+				option4: "Intubate and ventilate",
+				answer: 3
+			},
+			{
+				question: "With regards to obstructive sleep apnoea (OSA), which of the following statements is INCORRECT?",
+				option1: "Hhypoxaemia is the main stimulus to arousal",
+				option2: "Main method of treating this syndrome is with Continuous Positive Airway Pressure (CPAP)",
+				option3: "This syndrome occurs in up to 5% of adults",
+				option4: "This syndrome rarely has an obstructive component",
+				answer: 4
+			}
+		]
+	];
